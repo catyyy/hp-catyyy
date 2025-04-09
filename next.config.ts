398ade4857688de95cfd94cf8d1d063de1fd5063
catyyy <zongyejian@gmail.com>
2025-04-09 18:17:04 +0900

    hahahahhaha

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/nextjs-github-pages",
-  images: {
-    unoptimized: true,
-  },
+  
 };
 
 export default nextConfig;
