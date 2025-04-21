4ea3f20423286b1089390f514ddfa4c07a3c7dc0
catyyy <zongyejian@gmail.com>
2025-04-21 14:05:13 +0900

    fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import FloatingCards from "@/components/FloatingCards";
 import Navbar from "@/components/Navbar";
 import NetworkGraphComponent from "@/components/NetworkGraph";
 import { useRouter } from "next/navigation";
